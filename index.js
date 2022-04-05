36aa370f35c189c511999d9e3596eb5464f657a0
Zane Stock <zanestock3@gmail.com>
2022-04-05 07:55:24 -0500

    commit tree faf5bfea056ba6c877acdbb884f62915546d416b

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return "This one is on me!";
+  }
+  else if (someValue > 400 && someValue < 2000) {
+    return "That will be twenty bucks.";
+  }
+  else if (someValue > 2000 && someValue < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (someValue > 2500) {
+    return "No can do.";
+  }
+} 
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  return (city === "NYC") ? "Ok, sounds good." : "No go."
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ switch (tip) {
+   case 'generous':
+     return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    case 'thanks for everything':
+      default: return "Bye.";
+      
+ }
 }
